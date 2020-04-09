7b417c5b6f4dd9530dd7efc7b365c9db05051cc8
Roman <cypherpanda@pm.me>
2020-04-09 11:24:19 +0200

    EU Population density projection

--- pokenalysis.py
+++ pokenalysis.py
@@ -1,12 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/pokemon_data.csv')
+df = pd.read_csv('data/LF_415_NUTS0.csv')
+
+shape = df.head
 
 
-# df.to_html('pokemon.html', index=False)
-
-plot = df.plot.scatter(x= 'Attack',
-                       y= 'Defense',
-                       alpha= 0.5)
+ax = df.plot.scatter(x='NUTS_0',
+                       y='2010',
+                       alpha=1,
+                       color='DarkBlue',
+                       label='2010')
+df.plot.scatter(x='NUTS_0',
+                y='2020',
+                alpha=0.5,
+                color='Green',
+                label='2020',
+                ax=ax)
+df.plot.scatter(x='NUTS_0',
+                y='2030',
+                alpha=0.4,
+                color='Grey',
+                label='2030',
+                ax=ax)
+df.plot.scatter(x='NUTS_0',
+                y='2040',
+                alpha=0.3,
+                color='Grey',
+                label='2040',
+                ax=ax)
+df.plot.scatter(x='NUTS_0',
+                y='2050',
+                alpha=0.2,
+                color='Grey',
+                label='2050',
+                ax=ax)
 plt.show()
+
+# scatter alpha=0.5, hexbin gridsize=25,
+
+print(shape)
