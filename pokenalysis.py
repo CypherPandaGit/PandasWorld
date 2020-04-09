f9b5b9354ac9404762296ebab4be3790ce8eda4f
Roman <cypherpanda@pm.me>
2020-04-09 12:46:48 +0200

    EU Population density projection

--- pokenalysis.py
+++ pokenalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+plt.rcParams["figure.figsize"] = [10, 10]
 df = pd.read_csv('data/LF_415_NUTS0.csv')
 
 shape = df.head
@@ -10,7 +11,9 @@ ax = df.plot.scatter(x='NUTS_0',
                        y='2010',
                        alpha=1,
                        color='DarkBlue',
-                       label='2010')
+                       label='2010',
+                     title='EU Population projection')
+
 df.plot.scatter(x='NUTS_0',
                 y='2020',
                 alpha=0.5,
@@ -35,8 +38,12 @@ df.plot.scatter(x='NUTS_0',
                 color='Grey',
                 label='2050',
                 ax=ax)
+
+ax.set_xlabel('STATES')
+ax.set_ylabel('DENSITY')
+
 plt.show()
 
 # scatter alpha=0.5, hexbin gridsize=25,
 
-print(shape)
+# print(shape)
